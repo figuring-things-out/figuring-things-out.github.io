8e017e5b0f6468314f72ad4be32bbb58dc6293af
Figuring Things Out <gofigurethingsout@gmail.com>
2023-01-23 15:52:23 -0700

    Speeding things up

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
-$(window).on('beforeunload', function() {
-    $(window).scrollTop(0);
-  });
-  
-  window.addEventListener('load', () => {
-    const preload = document.querySelector('.preloader-wrapper');
-    const loading = document.querySelector('.loading');
-    setTimeout( function() {
-        preload.classList.add('preloader-wrapper-finish');
-        loading.classList.remove('loading');
-    }, 0 );
-  });
+window.addEventListener('load', () => {
+  const preload = document.querySelector('.preloader-wrapper');
+  const loading = document.querySelector('.loading');
+  setTimeout( function() {
+      preload.classList.add('preloader-wrapper-finish');
+      loading.classList.remove('loading');
+  }, 0 );
+});
